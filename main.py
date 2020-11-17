0677362ec9005783a976fe7fad636a57b358a30d
annusgit <annusgit@gmail.com>
2020-11-17 14:39:26 +0500

    working mqtt server with fault count for temperature sensor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from MQTT import MQTT
 import time
 
 
-server_end = ('192.168.0.120', 9999)
+server_end = ('192.168.8.122', 9999)
 node_end = ('', 8888)
 CURRENT_SENSOR_RATINGS = ['NONE', 5, 10, 15, 20, 25, 30, 50, 60, 100, 250]
 SSN_DEFAULT_CONFIGS = [9, 90, 10, 9, 95, 6, 6, 27, 2, 6, 28, 3, 1]
